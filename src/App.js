f0a8833e759ec7739b0891f17f62d7d95a4451e4
YCLe2 <chany4430@gmail.com>
2024-10-28 11:00:21 +0900

    change title

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          20241028 react
         </p>
         <Component1 />
         <a
