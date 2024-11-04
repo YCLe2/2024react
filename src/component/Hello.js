5e5d78e60083a07f5fe9637a22d28dd9c9f0bfb6
YCLe2 <chany4430@gmail.com>
2024-11-04 10:57:51 +0900

    hello.js

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -2,8 +2,11 @@ import './Hello.css';
 
 function Hello(props) {
     return (
-        <h1>Hello World!!!</h1>
+        <>
+            <h1>Hello World!!!</h1>
+            <h2>22000541</h2>
+        </>
     );
-}
+}                                   
 
 export default Hello;
